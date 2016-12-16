fcbbf02b0749f964ceecec435343aafea4a54b16
alexv7 <alexvarjabedian7@gmail.com>
2016-12-16 14:11:06 -0500

    authenticate_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,8 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  def authenticate_user!
+    redirect_to login_url, alert: "You need to be signed in before continuing" if current_user.nil?
+  end
 end
