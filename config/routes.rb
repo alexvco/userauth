39f891448485e856e443f015111cf2df54100479
alexv7 <alexvarjabedian7@gmail.com>
2016-12-16 12:58:11 -0500

    routes for sessions/new

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  resources :users
   root 'users#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+
+  match 'login'  =>  'sessions#new',  via: [:get],  :as => "login"
+
 end
