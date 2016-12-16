afb377005597aeeb65d5871520ca84c44e003784
alexv7 <alexvarjabedian7@gmail.com>
2016-12-16 14:01:51 -0500

    User logging out

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root 'users#index'
   resources :users
 
-  match 'login'  =>  'sessions#new',  via: [:get],  :as => "login_path"
+  match 'login'  =>  'sessions#new',  via: [:get],  :as => "login"
   match 'login'  =>  'sessions#create',  via: [:post]
+  match 'logout'  =>  'sessions#destroy',  via: [:delete], :as => "logout"
 
 end
