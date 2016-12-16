b58662a3e40558055cf7a133ab31f4500ae67835
alexv7 <alexvarjabedian7@gmail.com>
2016-12-16 12:01:12 -0500

    change password and password_confirmation in users controller params, user validations, new_user_path in nav, css for forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ApplicationRecord
   has_secure_password
+  
+  ##============================================================##
+  ## Validation
+  ##============================================================##  
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
-  validates :email, presence: true
-  validates_format_of :email,:with => VALID_EMAIL_REGEX
+  ##============================================================##
+  ## Callbacks
+  ##============================================================##
+  before_save   :downcase_email
 end
