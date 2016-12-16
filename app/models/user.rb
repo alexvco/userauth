999df3a5b3ed3ea1d5bd309ed1ee67de4b912119
alexv7 <alexvarjabedian7@gmail.com>
2016-12-16 11:11:47 -0500

    validates email format (note that has_secure_password inherently has password validations)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+  validates :email, presence: true
+  validates_format_of :email,:with => VALID_EMAIL_REGEX
 end
