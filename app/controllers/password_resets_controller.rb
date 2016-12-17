79b100b6f1c3f1d75891debb0f0eec5669036e3d
alexv7 <alexvarjabedian7@gmail.com>
2016-12-17 11:32:41 -0500

    password reset with update fully working

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -10,5 +10,24 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit
+    @user = User.find_by!(password_reset_token: params[:reset])
   end
+
+  def update
+    @user = User.find_by!(password_reset_token: params[:reset])
+    if @user.password_reset_sent_at < 2.hours.ago
+      redirect_to new_password_reset_path, alert: "Password reset has expired"
+    elsif @user.update_attributes(user_params)
+      redirect_to root_url, notice: "Password has been reset"
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation, :auth_token)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   match 'password_reset'  =>  'password_resets#new',  via: [:get],  :as => "new_password_reset"
   match 'password_reset'  =>  'password_resets#create',  via: [:post]
   match 'password_reset/:reset/edit'  =>  'password_resets#edit',  via: [:get], as: "edit_password_reset"
+  match 'password_reset/:reset/edit'  =>  'password_resets#update',  via: [:patch]
 
   match 'login'  =>  'sessions#new',  via: [:get],  :as => "login"
   match 'login'  =>  'sessions#create',  via: [:post]
