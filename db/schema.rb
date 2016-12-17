13192846d20cff449554181f19f9267a88bf433f
alexv7 <alexvarjabedian7@gmail.com>
2016-12-17 00:05:51 -0500

    rails g migration add_auth_token_to_users auth_token, rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161216151022) do
+ActiveRecord::Schema.define(version: 20161217050434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20161216151022) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "auth_token"
   end
 
 end
